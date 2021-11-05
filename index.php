e2545c84ba993ca8314e5ff10daed2a4e1eeccbb
MaciekMPL <maciekmalinowski12@gmail.com>
2021-11-05 18:49:04 +0100

    -style

--- index.php
+++ index.php
@@ -13,12 +13,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="arkusz.css">
-    <style>
-        html {
-            font-family: 'Quicksand', sans-serif;
-            --primary-color: #cfcfcf;
-        }
-    </style>
     <!-- menu,header-->
     <div class="c-header">
         <div class="c-header__wrapper">
