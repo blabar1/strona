e25e019c69c5b3d8d63a053c6a3b40d909b909b3
blabar1 <barejablazej@gmail.com>
2021-11-06 14:00:40 +0100

    slider

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const initSlider = () => {
     if (sliders.length > 0) {
         sliders.forEach(slider => {
             $(slider).slick({
-                dots: truegit ,
+                dots: true ,
                 prevArrow: '<i class="fas fa-angle-left c-banner-slider__arrow c-banner-slider__arrow--left"></i>',
                 nextArrow: '<i class="fas fa-angle-right c-banner-slider__arrow c-banner-slider__arrow--right"></i>'
             });
