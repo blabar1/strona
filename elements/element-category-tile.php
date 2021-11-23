777008ec77a64081af43d5fa6a574223b2b81210
blabar1 <barejablazej@gmail.com>
2021-11-23 19:34:42 +0100

    category finished

--- elements/element-category-tile.php
+++ elements/element-category-tile.php
@@ -1,48 +1,56 @@
 <div class="c-category-tile col-xs-6 col-sm-4 col-xl-4 col-lg-4 col-md-4">
     <div class="c-category-tile__past-hover">
-        <div class="c-category-tile__past-list">
-            <ul>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Laptopy i komputery</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Smartfony i smartwatche</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Gaming i streaming</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Podzespoły komputerowe</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Urządzenie peryferyjne</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">TV i audio</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Smarthome i lifestyle</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                        <div class="c-category-tile__past-hover-list">Akcesoria</div>
-                    </a>
-                </li>
-            </ul>
+        <div class="c-category-tile__past-list-container">
+            <div class="c-category-tile__past-list">
+                <ul>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Laptopy i komputery</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Smartfony i smartwatche</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Gaming i streaming</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Podzespoły komputerowe</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Urządzenie peryferyjne</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">TV i audio</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Smarthome i lifestyle</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Akcesoria</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <div class="c-category-tile__past-hover-list-element">Akcesoria</div>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     </div>
     <div class="c-category-tile__content">
