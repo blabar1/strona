902c8b60250af5ed9a163d4ee4325f3077a79cc2
blabar1 <barejablazej@gmail.com>
2021-11-23 18:16:24 +0100

    tile subcat

--- elements/element-category-tile.php
+++ elements/element-category-tile.php
@@ -1,8 +1,53 @@
 <div class="c-category-tile col-xs-6 col-sm-4 col-xl-4 col-lg-4 col-md-4">
-        <div class="c-category-tile__content">
-
-            <div class="c-category-tile__name"><?php echo $category; ?></div>
-            <div class="c-category-tile__thumbnail" style="background-image: url(<?php echo $thumbnail; ?>);"></div>
+    <div class="c-category-tile__past-hover">
+        <div class="c-category-tile__past-list">
+            <ul>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Laptopy i komputery</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Smartfony i smartwatche</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Gaming i streaming</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Podzespoły komputerowe</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Urządzenie peryferyjne</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">TV i audio</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Smarthome i lifestyle</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                        <div class="c-category-tile__past-hover-list">Akcesoria</div>
+                    </a>
+                </li>
+            </ul>
         </div>
-        <div class="test">lore</div>  
+    </div>
+    <div class="c-category-tile__content">
+
+        <div class="c-category-tile__name"><?php echo $category; ?></div>
+        <div class="c-category-tile__thumbnail" style="background-image: url(<?php echo $thumbnail; ?>);"></div>
+    </div>
 </div>
