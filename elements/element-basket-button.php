a24c495247a5a856091c3162b250b21653699d4b
MaciekMPL <maciekmalinowski12@gmail.com>
2021-11-29 12:01:57 +0100

    modal fix

--- elements/element-basket-button.php
+++ elements/element-basket-button.php
@@ -1,4 +1,4 @@
-<button name="<?php echo $id; ?>" data-toggle="modal" data-target="#<?php echo $id; ?>" class="c-product-title__button "><span class="c-basket__button-span"> <svg style="width: 100%;height: 100%;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<button name="<?php echo $id; ?>" data-toggle="modal" data-target="#m<?php echo $id; ?>" class="c-product-title__button "><span class="c-basket__button-span"> <svg style="width: 100%;height: 100%;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <g class="c-basket__button-svg">
                 <path d="M8 7v12h8V7H8zM7 6h10v14H7V6z" fill-rule="nonzero"></path>
                 <path d="M10 5v1h4V5h-4zM9 4h6v3H9V4z" fill-rule="nonzero"></path>
