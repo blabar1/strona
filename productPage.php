88566ec9a9f388ff6a56bda9ccaaab7c0fcf8133
MaciekMPL <maciekmalinowski12@gmail.com>
2021-12-03 22:38:42 +0100

    product page fix

--- productPage.php
+++ productPage.php
@@ -17,13 +17,22 @@
                     $idp=69;
                 $query = $conn->query("SELECT wartosc, nazwa FROM produkt_wlasciwosc INNER JOIN  wlasciwosc ON nazwa_wlasciwosc = id_wlasciwosc WHERE produkt = $idp ORDER BY nazwa LIMIT 4");
                 $results = $query->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($results as $row) {
-                    $wlasciowsci[] = $row['nazwa'] . ": " . $row['wartosc'];
+                if (!empty($results)) {
+                    foreach ($results as $row) {
+                        $wlasciowsci[] = $row['nazwa'] . ": " . $row['wartosc'];
+                    }
+                }else{
+                    $wlasciowsci[] = "Nie znaleziono.";
                 }
                 $query = $conn->query("SELECT * FROM galeria_zdjec WHERE produkt = $idp");
                 $result = $query->fetchALL(PDO::FETCH_ASSOC);
-                foreach($result as $row){
-                    $images[] = "images/produkty/$idp/".$row['plik'];
+                if (!empty($result)) {
+                    foreach($result as $row){
+                        $images[] = "images/produkty/$idp/".$row['plik'];
+                    }
+                }
+                else{
+                    $images[]='images/komputerek.jpg';
                 }
                 $query = $conn->query("SELECT * FROM produkt WHERE id_produkt = $idp LIMIT 1");
                 $result = $query->fetch(PDO::FETCH_ASSOC);
@@ -49,9 +58,13 @@
     <?php 
     $query1 = $conn->query("SELECT wartosc, nazwa FROM produkt_wlasciwosc INNER JOIN  wlasciwosc ON nazwa_wlasciwosc = id_wlasciwosc WHERE produkt = $idp ORDER BY nazwa");
     $result1 = $query1->fetchAll(PDO::FETCH_ASSOC);
-    foreach($result1 as $row1){
-        $specification[] =$row1['nazwa'] . ";" . $row1['wartosc'];
-    } 
+    if (!empty($result1)) {
+        foreach($result1 as $row1){
+            $specification[] =$row1['nazwa'] . ";" . $row1['wartosc'];
+        } 
+    }else{
+        $specification[] = "Błąd;Błąd";
+    }
     get_element("elements/element-product-specification.php", array(
         'specification' => $specification
         ));
